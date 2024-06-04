15cc14bdc8e7ff83b5b1ee07532b0ac5721e5276
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-06-04 19:53:31 +0100

    Performance improvement

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 
+import 'package:dice_flutter/dice_roller.dart';
 import 'package:dice_flutter/styled_text.dart';
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatefulWidget {
+class GradientContainer extends StatelessWidget {
   GradientContainer({
     super.key,
     this.colors,
@@ -12,50 +13,17 @@ class GradientContainer extends StatefulWidget {
   final List<Color>? colors;
 
   @override
-  State<GradientContainer> createState() => _GradientContainerState();
-}
-
-class _GradientContainerState extends State<GradientContainer> {
-  int diceNumber = 1;
-
-  void rollDice() {
-    // function body
-    setState(() {
-      diceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: widget.colors!,
+          colors: colors!,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-$diceNumber.png',
-              width: 200,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextButton(
-                onPressed: () {
-                  rollDice();
-                },
-                child: const Text(
-                  'Roll Dice !',
-                  style: TextStyle(color: Colors.white, fontSize: 28),
-                ))
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
