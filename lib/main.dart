c77139dfb59b83eb769f1f2f8cd1a7ce726395f4
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-06-04 14:52:21 +0100

    Widget Extraction GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -18,29 +18,40 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('First App'),
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF27085D), Color.fromARGB(255, 93, 9, 237)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+        body: const GradientContainer(),
+      ),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF27085D), Color.fromARGB(255, 93, 9, 237)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              style: TextStyle(color: Colors.white, fontSize: 28),
+              'Hello World!',
             ),
-          ),
-          child: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  style: TextStyle(color: Colors.white),
-                  'Hello World!',
-                ),
-                Text(
-                  style: TextStyle(color: Colors.white),
-                  'It\'s time to learn Flutter!',
-                ),
-              ],
+            Text(
+              style: TextStyle(color: Colors.white, fontSize: 28),
+              'It\'s time to learn Flutter!',
             ),
-          ),
+          ],
         ),
       ),
     );
